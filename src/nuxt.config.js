e9aaca7378de3712141624109987900396236bb7
nawawa <oonawa12@gmail.com>
2022-09-13 23:45:47 +0900

    ミドルウェアの場所が間違っていたのを修正

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -89,9 +89,9 @@ export default {
         }
       }
     },
-    router: {
-      middleware: ['auth'],
-    },
+  },
+  router: {
+    middleware: ['auth'],
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
