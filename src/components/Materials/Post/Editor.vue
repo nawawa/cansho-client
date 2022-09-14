33b5841fca03f977b7a49ff0e52cf8a87c3056eb
nawawa <oonawa12@gmail.com>
2022-09-14 18:06:03 +0900

    Tiptapのラップコンポーネント

---
+++ src/components/Materials/Post/Editor.vue
@@ -0,0 +1,54 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+
+export default {
+  components: {
+    EditorContent,
+  },
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+  },
+  emits: ['update:modelValue'],
+  data: () => ({
+    editor: null
+  }),
+  watch: {
+    modelValue(value) {
+      // HTML
+      const isSame = this.editor.getHTML() === value
+
+      // JSON
+      // const isSame = JSON.stringify(this.editor.getJSON()) === JSON.stringify(value)
+
+      if (isSame) {
+        return
+      }
+
+      this.editor.commands.setContent(value, false)
+    },
+  },
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        StarterKit,
+      ],
+      content: this.modelValue,
+      onUpdate: () => {
+        // HTML
+        this.$emit('update:modelValue', this.editor.getHTML())
+      },
+    })
+  },
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
