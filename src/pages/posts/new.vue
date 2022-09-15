8ba4ba8fa5bb183599a093f732694354f62a9702
nawawa <oonawa12@gmail.com>
2022-09-15 15:46:24 +0900

    HTMLのサニタイズ用モジュール動作確認

--- src/pages/posts/new.vue
+++ src/pages/posts/new.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
+    <!-- TODO：スタイルをつけるボタンの実装 エディタ全体のデザイン -->
     <editor v-model="content" />
-    <v-btn @click="put">
-      出力
-    </v-btn>
+    <div v-html="$sanitize(content)"></div>
   </div>
 </template>
 
@@ -14,48 +13,8 @@ export default {
     Editor
   },
   data: () => ({
-    content: '<p>A Vue.js wrapper component for tiptap to use <code>v-model</code>.</p>',
+    title: '',
+    content: '',
   }),
-  methods: {
-    put() {
-      console.log(this.content)
-    }
-  }
 }
 </script>
-
-<style lang="scss">
-/* Basic editor styles */
-.ProseMirror {
-  > * + * {
-    margin-top: 0.75em;
-  }
-
-  code {
-    background-color: rgba(#616161, 0.1);
-    color: #616161;
-  }
-}
-
-.content {
-  padding: 1rem 0 0;
-
-  h3 {
-    margin: 1rem 0 0.5rem;
-  }
-
-  pre {
-    border-radius: 5px;
-    color: #333;
-  }
-
-  code {
-    display: block;
-    white-space: pre-wrap;
-    font-size: 0.8rem;
-    padding: 0.75rem 1rem;
-    background-color:#e9ecef;
-    color: #495057;
-  }
-}
-</style>
