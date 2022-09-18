62332655ee8ae6b3c4f07cd273dc12f1185b66ed
nawawa <oonawa12@gmail.com>
2022-09-18 18:23:30 +0900

    ページコンポーネントのディレクトリ分け・タイトル入力追加

---
+++ src/components/Pages/Posts/New.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- TODO：スタイルをつけるボタンの実装 エディタ全体のデザイン -->
+    <client-only>
+      <textarea contenteditable="true" placeholder="記事タイトル" v-model="title"></textarea>
+      <editor v-model="content" />
+      <div v-html="$sanitize(content)"></div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+import Editor from '~/components/Materials/Post/Editor/Index.vue'
+export default {
+  components: {
+    Editor
+  },
+  data: () => ({
+    title: '',
+    content: '',
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+  textarea {
+    width: 100%;
+    height: 50px;
+    font-size: 32px;
+    line-height: 1.5;
+    resize: none;
+    outline-style: none;
+    caret-color: white;
+  }
+</style>
--- src/pages/posts/new.vue
+++ src/pages/posts/new.vue
@@ -1,22 +1,8 @@
 <template>
-  <div>
-    <!-- TODO：スタイルをつけるボタンの実装 エディタ全体のデザイン -->
-    <client-only>
-      <editor v-model="content" />
-      <div v-html="$sanitize(content)"></div>
-    </client-only>
-  </div>
+  <PagesPostsNew />
 </template>
 
 <script>
-import Editor from '~/components/Materials/Post/Editor.vue'
 export default {
-  components: {
-    Editor
-  },
-  data: () => ({
-    title: '',
-    content: '',
-  }),
 }
 </script>
