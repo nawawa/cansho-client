eb0b65bd8c9d140406dbd58b8f54fadd8c02108a
nawawa <oonawa12@gmail.com>
2022-09-19 11:00:31 +0900

    タイトル入力欄

--- src/components/Pages/Posts/New.vue
+++ src/components/Pages/Posts/New.vue
@@ -2,7 +2,16 @@
   <div>
     <!-- TODO：スタイルをつけるボタンの実装 エディタ全体のデザイン -->
     <client-only>
-      <textarea contenteditable="true" placeholder="記事タイトル" v-model="title"></textarea>
+      <textarea 
+        id="post-title"
+        contenteditable="true" 
+        placeholder="記事タイトル" 
+        v-model="title"
+        wrap="off"
+        @keydown.enter.prevent="cancel"
+        @compositionstart="composing=true"
+        @compositionend="composing=false"
+      ></textarea>
       <editor v-model="content" />
     </client-only>
   </div>
@@ -15,14 +24,27 @@ export default {
     Editor
   },
   data: () => ({
+    composing: false,
     title: '',
     content: '',
   }),
+  methods: {
+    cancel(e) {
+      if (this.composing) {
+        return 
+      } else {
+        const textarea = document.getElementById('post-title')
+        const editor = document.getElementsByClassName('ProseMirror')[0]
+        textarea.blur()
+        editor.focus()
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  textarea {
+  #post-title {
     width: 100%;
     height: 50px;
     font-size: 32px;
@@ -30,9 +52,13 @@ export default {
     resize: none;
     outline-style: none;
     caret-color: white;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    letter-spacing: 0.04em;
   }
-  textarea::placeholder {
+  #post-title::placeholder {
     color: white;
     opacity: 0.5;
+    font-family: 'Noto Sans JP', sans-serif !important;
   }
 </style>
