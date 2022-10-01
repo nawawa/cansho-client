5e54e521d13a7d98ca9182228d465213f2317e97
nawawa <oonawa12@gmail.com>
2022-10-01 15:04:32 +0900

    左側に固定表示する編集ボタンの親要素

---
+++ src/components/Materials/Post/Editor/ButtonContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  div {
+    padding: 8px;
+    width: 10px;
+    height: 10px;
+    position: fixed;
+    top: 50%;
+    transform: translate(0, -50%);
+  }
+</style>
