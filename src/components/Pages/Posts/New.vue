790ba3bd591c6ca08381d02a2dd2b53cbdc76d82
nawawa <oonawa12@gmail.com>
2022-10-01 15:03:09 +0900

    エディターコンポーネントにタイトル入力欄を統合

--- src/components/Pages/Posts/New.vue
+++ src/components/Pages/Posts/New.vue
@@ -2,29 +2,19 @@
   <div>
     <!-- TODO：スタイルをつけるボタンの実装 エディタ全体のデザイン -->
     <client-only>
-      <title-column v-model="title" @enter="focusOnEditor" />
       <editor v-model="content" />
     </client-only>
   </div>
 </template>
 
 <script>
-import TitleColumn from '~/components/Materials/Post/Editor/Title.vue'
 import Editor from '~/components/Materials/Post/Editor/Index.vue'
 export default {
   components: {
     Editor,
-    TitleColumn
   },
   data: () => ({
-    title: '',
     content: '',
   }),
-  methods: {
-    focusOnEditor() {
-      const editor = document.getElementsByClassName('ProseMirror')[0]
-      editor.focus()
-    }
-  }
 }
 </script>
