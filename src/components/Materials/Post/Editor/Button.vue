0547325a367d3acd8e36b0cdbb5e7442734987c8
nawawa <oonawa12@gmail.com>
2022-10-05 14:30:09 +0900

    装飾ボタン用コンポーネント

---
+++ src/components/Materials/Post/Editor/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn 
+    class="my-3"
+    :class="{ 'is-active': editor.isActive(buttonType) }"
+    @click="$emit('click')"
+    tile
+    icon
+  >
+    <v-icon>
+      <slot />
+    </v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonType: String,
+    editor: Object
+  }
+}
+</script>
