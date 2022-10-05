b7ef394fea8ddd398cce82d530158813b4f907c3
nawawa <oonawa12@gmail.com>
2022-10-05 14:36:25 +0900

    ツールバーのスタイルコンポーネント

---
+++ src/components/Materials/Post/Editor/Toolbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="toolbar-div" :style="styleAttribute">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    styleAttribute: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  #toolbar-div {
+    position: absolute;
+    opacity: 0;
+  }
+</style>
