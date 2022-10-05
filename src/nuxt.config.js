1ee67eeecdaadf06861c57b2783e130829570acd
nawawa <oonawa12@gmail.com>
2022-10-05 14:28:27 +0900

    エディタと記事の画面用CSSを別ファイルへ分割

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -25,6 +25,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     { src: '~/assets/scss/common.scss' },
+    { src: '~/assets/scss/post.scss' },
   ],
   
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
