86865b40a663c2048076298e86792cb95c16d208
nawawa <oonawa12@gmail.com>
2022-11-05 14:45:07 +0900

    メニューボタン：固定のCSSをインラインからスタイルタグ内へ移動・left の指定を追加

--- src/components/Materials/Post/Editor/MenuButton.vue
+++ src/components/Materials/Post/Editor/MenuButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button aria-label="メニューを開く" :style="`width: 100px; height:100px; position:absolute; background:black; top: ${rectY}px;`">
+  <button aria-label="メニューを開く" :style="`top: ${rectY}px; left: ${rectX}px`">
     <div />
   </button>
 </template>
@@ -7,11 +7,17 @@
 <script>
 export default {
   props: {
-    rectY: Number
+    rectY: Number,
+    rectX: Number
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  button {
+    width: 40px; 
+    height: 40px; 
+    position: absolute; 
+    background: black;
+  }
 </style>
