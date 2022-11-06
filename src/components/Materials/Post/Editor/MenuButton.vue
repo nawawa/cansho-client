7bd708579a5570f702c9a233d78660aa0bde6008
nawawa <oonawa12@gmail.com>
2022-11-06 13:49:18 +0900

    黒の四角形で仮に作ってたメニューボタンをSVGへ差し替え

--- src/components/Materials/Post/Editor/MenuButton.vue
+++ src/components/Materials/Post/Editor/MenuButton.vue
@@ -1,6 +1,10 @@
 <template>
-  <button aria-label="メニューを開く" :style="`top: ${rectY}px; left: ${rectX}px`">
-    <div />
+  <button aria-label="メニューを開く" :style="`top: ${rectY}px;`">
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10.1588 19.6825H29.2064" stroke="white" stroke-width="2" stroke-linecap="square"/>
+      <path d="M19.6826 29.2063L19.6826 10.1587" stroke="white" stroke-width="2" stroke-linecap="square"/>
+      <circle cx="20" cy="20" r="19.5" stroke="white"/>
+    </svg>
   </button>
 </template>
 
@@ -8,7 +12,6 @@
 export default {
   props: {
     rectY: Number,
-    rectX: Number
   }
 }
 </script>
@@ -18,6 +21,6 @@ export default {
     width: 40px; 
     height: 40px; 
     position: absolute; 
-    background: black;
+    margin-left: -60px;
   }
 </style>
