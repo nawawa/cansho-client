e57859f86e9ff6b71f311f66001e8914d2637f6d
nawawa <oonawa12@gmail.com>
2022-11-12 17:55:49 +0900

    自前で実装したツールバーをBubbleMenuへ差し替え

---
+++ src/components/Materials/Post/Editor/BubbleMenuContentContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="toolbar-div">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    styleAttribute: String,
+    classAttribute: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  #toolbar-div {
+    background: var(--v-main-base);
+    filter: drop-shadow(10px 10px 10px rgba(0,0,0,0.3));
+  }
+</style>
---
+++ src/components/Materials/Post/Editor/MenuButton/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :style="`width: ${width}; height: ${height};`" aria-label="メニューを開く">
+    <svg :width="width" :height="height" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10.1588 19.6825H29.2064" stroke="white" stroke-width="2" stroke-linecap="square"/>
+      <path d="M19.6826 29.2063L19.6826 10.1587" stroke="white" stroke-width="2" stroke-linecap="square"/>
+      <circle cx="20" cy="20" r="19.5" stroke="white"/>
+    </svg>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    width: Number,
+    height: Number
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  button {
+    position: absolute; 
+  }
+</style>
