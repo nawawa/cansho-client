b82b46014b36f77624d32c536cb21124e7f900b2
nawawa <oonawa12@gmail.com>
2022-11-17 15:51:37 +0900

    メニューリストのスタイルを調整

--- src/components/Materials/Post/Editor/Menu/List.vue
+++ src/components/Materials/Post/Editor/Menu/List.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="menu-list-wrapper">
-    <v-row v-for="menu in menus" :key="menu.index">
-      <v-col>
+    <h1>挿入</h1>
+    <v-row v-for="menu in menus" :key="menu.index" class="py-2">
+      <v-col cols="1" class="py-0">
         <v-icon>
           mdi-{{ menu.type }}
         </v-icon>
       </v-col>
-      <v-col>
+      <v-col class="py-0 pl-8">
         <p>{{ menu.name }}</p>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col>
+    <v-row class="py-2">
+      <v-col cols="1" class="py-0">
         <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="white" xmlns:xlink="http://www.w3.org/1999/xlink">
           <path d="M18 6H6V9H4V4H20V9H18V6Z"></path>
           <path d="M4 15H6V18H18V15H20V20H4V15Z"></path>
           <path d="M2 11H22V13H2V11Z"></path>
         </svg>
       </v-col>
-      <v-col>
+      <v-col class="py-0 pl-8">
         <p>区切り線</p>
       </v-col>
     </v-row>
@@ -41,11 +42,22 @@ export default {
 
 <style lang="scss" scoped>
   #menu-list-wrapper {
+    border: 1px solid white;
+    border-radius: 4px;
     margin-left: 50px;
-    padding: 20px 0;
+    padding: 10px;
+    background: var(--v-main-base);
+  }
+
+  h1 {
+    font-size: 14px !important;
+    padding-bottom: 12px;
+    font-weight: normal;
   }
 
   p {
     margin: 0;
+    padding-top: 2.3px;
+    font-size: 14px !important;
   }
 </style>
