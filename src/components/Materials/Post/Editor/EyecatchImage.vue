47858f3b3277d143cc1ae6fc86ae4dad09b2bb2c
nawawa <oonawa12@gmail.com>
2022-11-22 16:23:21 +0900

    アイキャッチ追加ボタンのスタイル調整・データバインディングの用意

--- src/components/Materials/Post/Editor/EyecatchImage.vue
+++ src/components/Materials/Post/Editor/EyecatchImage.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <v-btn icon outlined>
-      <v-icon small>mdi-image-plus-outline</v-icon>
+    <v-btn 
+      v-if=" eyecatchImage.alt === '' && eyecatchImage.src === '' "
+      icon 
+      outlined
+      color="#adb5bd"
+    >
+      <v-icon 
+        small
+        color="#adb5bd"
+      >
+        mdi-image-plus-outline
+      </v-icon>
     </v-btn>
     <!-- <figure>
-
+      <img>
     </figure> -->
   </div>
 </template>
 
 <script>
 export default {
+  model: {
+    prop: "eyecatchImage",
+  },
+  props: {
+    eyecatchImage: {
+      alt: String,
+      src: String
+    }
+  }
 }
 </script>
