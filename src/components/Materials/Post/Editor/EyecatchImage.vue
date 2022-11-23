ce5dc9e3bfd1c8554dcdfe93f7e7926033052b8f
nawawa <oonawa12@gmail.com>
2022-11-23 10:44:32 +0900

    少しリファクタリング

--- src/components/Materials/Post/Editor/EyecatchImage.vue
+++ src/components/Materials/Post/Editor/EyecatchImage.vue
@@ -30,13 +30,15 @@ export default {
       console.log(this.$config.NODE_ENV)
 
       if (this.$config.NODE_ENV === 'local') {
+        // バックエンドAPIでアップロードするので、ローカルでは何もしない
+        // Picsum でランダムに画像をとってくる
+
         console.log('ローカル')
+        return this.$emit("input", 'https://picsum.photos/800/418');
+
       } else {
         // アップロードする
       }
-
-      const srcValue = 'https://picsum.photos/800/418' 
-      this.$emit("input", srcValue);
     }
   },
 }
